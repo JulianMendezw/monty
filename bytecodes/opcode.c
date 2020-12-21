c4f38358db02d23417b39d4b171b8dcb1fe5347d
Julian <julianlx@hotmail.com>
2020-12-21 09:19:52 -0500

    adding function _push

--- bytecodes/opcode.c
+++ bytecodes/opcode.c
@@ -1,34 +1,78 @@
 #include "monty.h"
-#include <stdlib.h>
 
 /**
- * add_dnodeint - A function that add a new node to the beginning list.
- *@head: Pointer to the first node.
- *@n: Data to set in a new node.
- * Return: A new node.
+ * _push - A function that add a new node to the beginning of a list.
+ * @head: Pointer to the first node.
+ * @line_number: Is the line number where the instruction appears
  */
 
 void _push(stack_t **head, unsigned int line_number)
 {
+	(void)head;
 	stack_t *node = NULL;
 	char *data = NULL;
-	int n = 0;
+	int n = 0, i = 0;
+
+	data = strtok(NULL, " ");
+	if (!data)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		_free_all_varx();
+		exit(EXIT_FAILURE);
+	}
+
+	for (i = 0; data[i]; i++)
+	{
+		if (data[i] < 48 || data[i] > 57)
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", line_number),
+			_free_all_varx(),
+			exit(EXIT_FAILURE);
+		}
+	}
 
 	node = malloc(sizeof(stack_t));
 	if (!node)
 		fprintf(stderr, "Error: malloc failed\n"),
 		exit(EXIT_FAILURE);
 
-	if (*head)
-		(*head)->prev = node;
-	data = strtok(NULL, " ");
+	if (varx.head)
+		(varx.head)->prev = node;
 
-	//add error handling
+	printf("New node created, data: (%s)\n", data);
 	n = atoi(data);
 
 	node->n = n;
-	node->next = *head;
+	node->next = varx.head;
 	node->prev = NULL;
-	*head = node;
+	varx.head = node;
 	printf("->Node added %d\n", line_number);
+}
+
+
+/**
+ * _free - A function that frees a stack_t list.
+ */
+
+void _free(void)
+{
+	stack_t *current = NULL;
+
+	while (varx.head)
+		current = varx.head,
+		varx.head = varx.head->next,
+		free(current);
+}
+
+/**
+ * _free_all_varx - A function that frees all variables.
+ */
+
+void _free_all_varx(void)
+{
+	if (varx.buffer)
+		free(varx.buffer);
+
+	_free();
+	fclose(varx.fd);
 }
