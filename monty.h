b18258729c52a51e798ce2134355fc1c042654c5
Julian <julianlx@hotmail.com>
2020-12-22 12:29:47 -0500

    adding new prototype _sub

--- monty.h
+++ monty.h
@@ -71,5 +71,6 @@ void _swap(stack_t **head, unsigned int line_number);
 /** File: opcode_1.c */
 void _add(stack_t **head, unsigned int line_number);
 void _nop(stack_t **head, unsigned int line_number);
+void _sub(stack_t **head, unsigned int line_number);
 
 #endif
