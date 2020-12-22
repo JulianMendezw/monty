27ebbc6bf40f428c7f9316b79f96267d374f3db5
Julian <julianlx@hotmail.com>
2020-12-21 23:45:25 -0500

    adding new function nop

--- monty.h
+++ monty.h
@@ -61,10 +61,14 @@ void _get_info_data(void);
 void _get_opcode(unsigned int line_number);
 void _free_all_varx(void);
 
-/** File: opcode.c */
+/** File: opcode_0.c */
 void _push(stack_t **head, unsigned int line_number);
 void _pall(stack_t **head, unsigned int line_number);
 void _pint(stack_t **head, unsigned int line_number);
 void _pop(stack_t **head, unsigned int line_number);
+void _swap(stack_t **head, unsigned int line_number);
+
+/** File: opcode_1.c */
+void _nop(stack_t **head, unsigned int line_number);
 
 #endif
