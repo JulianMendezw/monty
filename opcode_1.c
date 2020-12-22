5a64cb22cd3f478fd3c4354bc6e71989e01330c8
Julian <julianlx@hotmail.com>
2020-12-22 12:24:14 -0500

    adding new function _add

--- opcode_1.c
+++ opcode_1.c
@@ -1,6 +1,35 @@
 #include "monty.h"
 
 /**
+ * _add - A function that do anything..
+ * @head: Pointer to the first node.
+ * @line_number: Is the line number where the instruction appears
+ */
+
+void _add(stack_t **head, unsigned int line_number)
+{
+	int add = 0;
+	stack_t *tmp = *head;
+
+
+	if (!*head || !(*head)->next)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		_free_all_varx();
+		exit(EXIT_FAILURE);
+	}
+
+	add = (*head)->n + (*head)->next->n;
+
+	*head = (*head)->next;
+	(*head)->n = add;
+	(*head)->prev = NULL;
+
+	free(tmp);
+
+}
+
+/**
  * _nop - A function that do anything..
  * @head: Pointer to the first node.
  * @line_number: Is the line number where the instruction appears
@@ -10,5 +39,4 @@ void _nop(stack_t **head, unsigned int line_number)
 {
 	(void)head;
 	(void)line_number;
-
 }
