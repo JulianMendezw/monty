66f952b051dbeb6d222f2c1de4a9d1cc258b1a00
Julian <julianlx@hotmail.com>
2020-12-22 14:47:56 -0500

    chanching order in while line 76 & add break

--- opcode_2.c
+++ opcode_2.c
@@ -73,8 +73,11 @@ void _pstr(stack_t **head, unsigned int line_number)
 	stack_t *tmp = *head;
 	(void)line_number;
 
-	while (tmp && tmp->n != 0 && (tmp->n > 0 || tmp->n <= 127))
+	while (tmp &&  (tmp->n > 0 || tmp->n <= 127))
 	{
+		if (tmp->n == 0)
+		break;
+
 		putchar(tmp->n);
 		tmp = tmp->next;
 	}
