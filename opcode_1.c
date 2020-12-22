37e6553fdfad6b7b2a6ca96c5a552622b67c7457
Julian <julianlx@hotmail.com>
2020-12-21 23:53:05 -0500

    adding nop func

---
+++ opcode_1.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * _nop - A function that do anything..
+ * @head: Pointer to the first node.
+ * @line_number: Is the line number where the instruction appears
+ */
+
+void _nop(stack_t **head, unsigned int line_number)
+{
+	(void)head;
+	(void)line_number;
+}
